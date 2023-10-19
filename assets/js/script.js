e584b8323acb9b9a1f68830eb27c9241e651acd3
Krystopher88 <bichon.christ@gmail.com>
2023-10-19 10:25:37 +0200

    Ajout website & animation

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const link = document.getElementById("popupLink");
+  const popup = document.getElementById("popup");
+  const overlay = document.getElementById("overlay");
+  const closePopup = document.getElementById("closePopup");
+
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    popup.style.display = "block";
+    overlay.style.display = "block";
+  });
+
+  overlay.addEventListener("click", function () {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+    // Faites défiler vers le haut de la page
+    window.scrollTo(0, 0);
+  });
+
+  closePopup.addEventListener("click", function () {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+    // Faites défiler vers le haut de la page
+    window.scrollTo(0, 0);
+  });
+});
